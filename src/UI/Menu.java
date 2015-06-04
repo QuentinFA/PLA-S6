4ae00af7814d17f73d02a303f3b1029bc5f940f5
FlorianPO <florian.popek@gmail.com>
2015-06-04 16:40:09 +0200

    Commit Zialong

--- src/UI/Menu.java
+++ src/UI/Menu.java
@@ -11,7 +11,26 @@ import Game.Ressources;
 
 public class Menu 
 {
+	static boolean isOnSprite(Sprite s)
+	{
+		FloatRect rectangle = s.getGlobalBounds();
+		
+		Vector2i pos_mouse = Mouse.getPosition();
+		Vector2i real_pos = Vector2i.add(pos_mouse, Graphic.SFML.getPositionCamera());
+		
+		if (real_pos.x > rectangle.left && 
+				real_pos.x < (rectangle.left+rectangle.width) &&
+				   real_pos.y > rectangle.top &&
+				     real_pos.y < (rectangle.top+rectangle.height))
+				return true;
+		return false;
+	}
+	
 	Sprite bouton = new Sprite();
+	Sprite boutonSound = new Sprite();
+	
+	
+	
 	public static Menu MENU = null;
 	public static void init()
 	{
@@ -29,19 +48,16 @@ public class Menu
 	}
 	public boolean gerer()
 	{
-		Vector2i pos_mouse = Mouse.getPosition();
-		Vector2i real_pos = Vector2i.add(pos_mouse, Graphic.SFML.getPositionCamera());
-		
-		FloatRect rectangle = bouton.getGlobalBounds();		
-		
 		if (Mouse.isButtonPressed(Button.LEFT))
-				if (real_pos.x > rectangle.left && 
-						real_pos.x < (rectangle.left+rectangle.width) &&
-						   real_pos.y > rectangle.top &&
-						     real_pos.y < (rectangle.top+rectangle.height))
-				{
+		{
+			if (isOnSprite(bouton)) //Play
 					return true;
-				}
+			if (isOnSprite(boutonSound))
+			{
+				//Change sound
+			}
+		}
+		
 		return false;
 	}
 }
