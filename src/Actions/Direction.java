224d3a122505308a17f872a3324ea685a1e3536e
QuentinFA <aoghar@gmail.com>
2015-06-04 16:06:47 +0200

    Nom plus approprié pour l'exception hors de la map, valeurs pour les directions.

---
+++ src/Actions/Direction.java
@@ -0,0 +1,9 @@
+package Actions;
+
+public abstract class Direction
+{
+	public final static int NORTH = 0;
+	public final static int WEST = 1;
+	public final static int SOUTH = 2;
+	public final static int EST = 3;
+}
