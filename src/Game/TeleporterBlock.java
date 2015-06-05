5406b244f5bba6a6283580014168b71261851fb3
banana38 <anna.bruel@orange.fr>
2015-06-05 12:05:34 +0200

    add teleporterBlock

--- src/Game/TeleporterBlock.java
+++ src/Game/TeleporterBlock.java
@@ -1,7 +1,29 @@
-package entities;
+package Game;
 
-public class Teleporter {
+import Actions.Color;
+import Actions.Coordonnees;
+
+public class TeleporterBlock extends Block{
+	private TeleporterBlock destination = null;
+	private int couleur;
 	
-	//private block;
-
+	public TeleporterBlock()
+	{
+		this.position = new Coordonnees(0,0,0);
+		this.couleur = Color.GRIS;
+	}
+	
+	public TeleporterBlock(Coordonnees entree, int coul)
+	{
+		entree = this.position;
+		coul = this.couleur;
+	}
+	
+	public void lier(TeleporterBlock copain){
+		this.destination = copain;
+	}
+	
+	Coordonnees getSortie(){
+		return destination.position;
+	}
 }
