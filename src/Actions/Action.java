3b9b9d754656312891c66044f69b985e1bf2919d
Fheanor <darkedwin38@hotmail.fr>
2015-06-05 16:43:58 +0200

    Ajout des couleurs pour les actions et correction de divers bugs
    Je te dédicace ce commit Quentin <3

--- src/Actions/Action.java
+++ src/Actions/Action.java
@@ -1,4 +1,5 @@
 package Actions;
+import Entities.Character;
 
 public abstract class Action 
 {
@@ -8,5 +9,15 @@ public abstract class Action
 	 * @param orientation : L'orientation pour effectuer l'action
 	 * @return : La coordonnées résultante
 	 */
+	
+	protected int c;
+	
+	public int getColor() {return c;}
+	
+	public void setColor(int color) {c = color;}
+	
 	public abstract Coordonnees execute(Coordonnees p, int orientation);
+	
+	//Pour left_turn et right_turn
+	public abstract void execute(Character p);
 }
