d4d356c5aef227b5118d320912aa078b78d7ff63
赵子龙 <zilong_zhao@foxmail.com>
2015-06-09 11:13:13 +0200

    no message

--- src/UI/Menu_Level.java
+++ src/UI/Menu_Level.java
@@ -15,7 +15,8 @@ public class Menu_Level extends Menu
 	ArrayList<Sprite> monde_list = new ArrayList<Sprite>();
 	ArrayList<ArrayList<Sprite>> nbr_level_list = new ArrayList<ArrayList<Sprite>>();
 
-	Sprite aura = new Sprite();
+	ArrayList<Sprite> aura_list = new ArrayList<Sprite>();
+	//private Sprite aura
 	private static float title_scale;
 	private boolean increase_title_scale;
 	private int nbr_monde = 0;
@@ -27,19 +28,22 @@ public class Menu_Level extends Menu
 	{	
 		Graphic.SFML.visible_cursor();
 		Sprite spr;
-        for(int i = 0 ; i < 2 ; i++)
-        {
-        	spr = new Sprite();
-    		spr.setTexture(Ressources.TEXTURE.getTexture(TEXTURE.MONDE));
-    		spr.setTextureRect(new IntRect(1+i*401, 1, 400, 400));
-    		spr.setOrigin(new Vector2f(spr.getTextureRect().width/2.f, spr.getTextureRect().height/2.f));
-    		spr.setPosition(Graphic.SFML.getCenterCamera());
-    		monde_list.add(spr);
-        }
-		
+		for(int i = 0 ; i < 2 ; i++)
+		{
+			spr = new Sprite();
+			spr.setTexture(Ressources.TEXTURE.getTexture(TEXTURE.MONDE));
+			spr.setTextureRect(new IntRect(1+i*401, 1, 400, 400));
+			spr.setOrigin(new Vector2f(spr.getTextureRect().width/2.f, spr.getTextureRect().height/2.f));
+			spr.setPosition(Graphic.SFML.getCenterCamera());
+			monde_list.add(spr);
+			spr = new Sprite();
 
-		aura.setTexture(Ressources.TEXTURE.getTexture(TEXTURE.AURA));
-		aura.setOrigin(Ressources.TEXTURE.getHalfSize(TEXTURE.AURA));
+			spr.setTexture(Ressources.TEXTURE.getTexture(TEXTURE.AURA));
+			spr.setOrigin(Ressources.TEXTURE.getHalfSize(TEXTURE.AURA));
+			aura_list.add(spr);
+
+		}
+
 		title_scale = 1.25f;
 		increase_title_scale = true;
 
@@ -52,7 +56,7 @@ public class Menu_Level extends Menu
 				spr.setTexture(Ressources.TEXTURE.getTexture(TEXTURE.NBR_LEVEL));
 				spr.setTextureRect(new IntRect(1+89*j, 1, 88, 88));
 				spr.setOrigin(new Vector2f(spr.getTextureRect().width/2.f, 0));
-				nbr_level_list.get(0).add(spr);
+				nbr_level_list.get(i).add(spr);
 			}
 		}
 
@@ -60,25 +64,29 @@ public class Menu_Level extends Menu
 		fleche_right.setTextureRect(new IntRect(1 , 1 , 100 , 100));
 		fleche_right.setOrigin(new Vector2f(fleche_right.getTextureRect().width/2.f, fleche_right.getTextureRect().height/2.f));
 		fleche_right.setPosition(Graphic.SFML.getCenterCamera().x + Graphic.SFML.getSizeCamera().x/2.f - fleche_right.getTextureRect().width, Graphic.SFML.getCenterCamera().y);
-	
+
 		fleche_left.setTexture(Ressources.TEXTURE.getTexture(TEXTURE.BOUTON_FLECHE));
 		fleche_left.setTextureRect(new IntRect(102 , 1 , 100 , 100));
 		fleche_left.setOrigin(new Vector2f(fleche_left.getTextureRect().width/2.f, fleche_left.getTextureRect().height/2.f));
 		fleche_left.setPosition(Graphic.SFML.getCenterCamera().x - Graphic.SFML.getSizeCamera().x/2.f + fleche_left.getTextureRect().width, Graphic.SFML.getCenterCamera().y);
-	
-	
-	
+
+
+
 	}
 
 	public void afficher()
 	{
 		if(nbr_monde<3)
-		Graphic.SFML.draw(fleche_right);
-		for (Sprite spr : monde_list)
+			Graphic.SFML.draw(fleche_right);
+		for(Sprite spr: aura_list)
 		{
-			Graphic.SFML.draw(aura);
 			Graphic.SFML.draw(spr);
 		}
+		for (Sprite spr : monde_list)
+		{
+			Graphic.SFML.draw(spr);
+		}
+		
 
 		for (ArrayList<Sprite> arr : nbr_level_list)
 			for (Sprite spr : arr)
@@ -92,10 +100,14 @@ public class Menu_Level extends Menu
 		for (int i = 0 ; i < monde_list.size(); i++)
 		{
 			monde_list.get(i).setPosition(new Vector2f((i-nbr_monde)*Graphic.SFML.getSizeCamera().x+Graphic.SFML.getCenterCamera().x , Graphic.SFML.getCenterCamera().y));
-		}
 
-		aura.setScale(title_scale, title_scale);
-		aura.setRotation(aura.getRotation()+1);
+		}
+		for(int i = 0; i < aura_list.size(); i++)
+		{
+			aura_list.get(i).setPosition(monde_list.get(i).getPosition());
+			aura_list.get(i).setScale(title_scale, title_scale);
+			aura_list.get(i).setRotation(aura_list.get(i).getRotation()+1);
+		}
 		if (increase_title_scale)
 		{
 			title_scale *= 1.01f;
@@ -133,7 +145,7 @@ public class Menu_Level extends Menu
 			{
 				if(nbr_monde<3)//nombre de niveau
 					nbr_monde++;
-	            
+
 
 			}
 			if(Graphic.isOnSprite(fleche_left))
@@ -143,7 +155,7 @@ public class Menu_Level extends Menu
 
 			}
 		}
-		
+
 
 	}
 }
