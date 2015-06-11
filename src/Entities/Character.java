b5b4fff9bdc7bb462f6604a60e71b4676bf843fb
FlorianPO <florian.popek@gmail.com>
2015-06-11 11:24:35 +0200

    no message

--- src/Entities/Character.java
+++ src/Entities/Character.java
@@ -12,11 +12,15 @@ public class Character extends Entities
 	private Procedure main;
 	private int orientation; //0: haut, 1: droite, 2: bas, 3: gauche, voir Orientation.java
 	private Color couleur;
+	private Action action_courant = null;
+	private Coordonnees moving_coord;
 	private Chest coffre;
 	
 	public Character(Coordonnees pos, int ori) 
 	{
 		coord = pos;
+		moving_coord = new Coordonnees(pos);
+		
 		orientation = ori;
 		couleur = Color.DEFAUT;
 		coffre = null;
@@ -40,24 +44,38 @@ public class Character extends Entities
 	public void setMain (Procedure m) {main = m;}
 	public Procedure getMain() {return main;}
 	
-	/*
-	public void use_Actions() 
-	{
-		Action a;
-		int i=0;
-		while (!lActions.isEmpty()) 
-		{
-			a=lActions.get(i);
-			lActions.remove(i);
-			i++;
-			this.use_Action(a);
-		}
-	}
-	*/
+	public void setMovingCoord(Coordonnees c) {moving_coord = c;}
+	public Coordonnees getMovingCoord() {return moving_coord;}
+	public void setTextureRect(IntRect rect) {sprite.setTextureRect(rect);}
 	
+	public boolean gerer() 
+	{
+		if (action_courant != null)
+		{
+			if (action_courant.execute(this))
+			{
+				action_courant = null;
+				return true;
+			}
+		}
+		return true;
+	}
+
 	/**
 	 *  Effectue l'action pour le personnage
 	 * @param a Action a effectuer par le personnage
+	 * @throws CloneNotSupportedException 
 	 */
-	public void use_Action(Action a) {a.execute(this);}
+	public void use_Action(Action a)
+	{
+		try
+		{
+			if (action_courant == null)
+				action_courant = (Action) a.clone();
+		}
+		catch (CloneNotSupportedException e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
