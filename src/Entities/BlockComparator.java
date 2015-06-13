413ae12e442fbe14a5b481b356f5db089cbc8e90
FlorianPO <florian.popek@gmail.com>
2015-06-13 12:37:58 +0200

    Rajout de BlockComparator (il n'y était plus)

---
+++ src/Entities/BlockComparator.java
@@ -0,0 +1,23 @@
+package Entities;
+
+import java.util.Comparator;
+
+public class BlockComparator implements Comparator<Block>
+{
+	@Override
+	public int compare(Block o1, Block o2)
+	{
+		double z = o1.getCoord().getZ() - o2.getCoord().getZ(), 
+				y = o1.getCoord().getY() - o2.getCoord().getY(), 
+				x = o1.getCoord().getX() - o2.getCoord().getX();
+		
+		if (z == 0)
+		{
+			if (y == 0)
+				return (int) x;
+			return (int) y;
+		}
+		
+		return (int) z;
+	}
+}
