87e05f8ceb3acfc147892622728d77d557f4fb4a
banana38 <anna.bruel@orange.fr>
2015-06-15 15:09:36 +0200

    dessin fork

---
+++ src/Prog/NormalActions/Clone.java
@@ -0,0 +1,17 @@
+package Prog.NormalActions;
+
+import Entities.Character;
+import Prog.Action;
+import Prog.Color;
+
+public class Clone extends Action{
+	
+	public Clone(Color c){couleur = c;}
+	
+	
+	public boolean execute(Character p) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
