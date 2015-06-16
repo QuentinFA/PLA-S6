42096f068ba53ccb8459520e9a180278db5d52cb
赵子龙 <zilong_zhao@foxmail.com>
2015-06-16 10:01:39 +0200

    no message

--- src/UI/Gui.java
+++ src/UI/Gui.java
@@ -225,9 +225,16 @@ public class Gui
 				if(spriteList_proc1.size()+spriteList_proc2.size()+spriteList_main.size() < World.WORLD.getMaxStar() &&
 						spriteList_proc1.size()+spriteList_proc2.size()+spriteList_main.size() > World.WORLD.getMinStar())
 				{
-					sprite_star.get(1).setTexture(Ressources.TEXTURE.getTexture(TEXTURE.STAR_FULL));
-					sprite_star.get(1).setOrigin(Ressources.TEXTURE.getHalfSize(TEXTURE.STAR_FULL));
-					sprite_star.get(1).setPosition(Graphic.SFML.getCenterCamera().x+(1-i)*200,Graphic.SFML.getCenterCamera().y-100);
+					if(i == 0)
+					{sprite_star.get(i).setTexture(Ressources.TEXTURE.getTexture(TEXTURE.STAR_EMPTY));
+					sprite_star.get(i).setOrigin(Ressources.TEXTURE.getHalfSize(TEXTURE.STAR_EMPTY));}
+					else
+					{sprite_star.get(i).setTexture(Ressources.TEXTURE.getTexture(TEXTURE.STAR_FULL));
+					sprite_star.get(i).setOrigin(Ressources.TEXTURE.getHalfSize(TEXTURE.STAR_FULL));}
+					sprite_star.get(i).setPosition(Graphic.SFML.getCenterCamera().x+(1-i)*200,Graphic.SFML.getCenterCamera().y-100);
+				}
+				else
+				{
 					sprite_star.get(i).setTexture(Ressources.TEXTURE.getTexture(TEXTURE.STAR_FULL));
 					sprite_star.get(i).setOrigin(Ressources.TEXTURE.getHalfSize(TEXTURE.STAR_FULL));
 					sprite_star.get(i).setPosition(Graphic.SFML.getCenterCamera().x+(1-i)*200,Graphic.SFML.getCenterCamera().y-100);
