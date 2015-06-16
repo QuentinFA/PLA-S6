da8e404fdf0ddbe684f7f8a5709a0baeaf315ceb
赵子龙 <zilong_zhao@foxmail.com>
2015-06-16 16:13:46 +0200

    maj les actions de chest

--- src/Entities/Blocks/Chests/ChestForward.java
+++ src/Entities/Blocks/Chests/ChestForward.java
@@ -1,6 +1,8 @@
 package Entities.Blocks.Chests;
 
 import org.jsfml.graphics.IntRect;
+import org.jsfml.graphics.Sprite;
+import org.jsfml.system.Vector2f;
 
 import Entities.Chest;
 import Game.Ressources;
@@ -8,20 +10,33 @@ import Game.Ressources.TEXTURE;
 import Prog.Color;
 import Prog.Coordonnees;
 import Prog.NormalActions.Forward;
+import UI.Graphic;
 
 public class ChestForward extends Chest 
 {
+	Sprite sprite_action = new Sprite();
 	public ChestForward(Coordonnees pos, int ori) 
 	{
 		this.coord = pos;
 		this.orientation = ori;
 		this.action = new Forward(Color.DEFAUT);
 		sprite.setTexture(Ressources.TEXTURE.getTexture(TEXTURE.CHEST));
+		sprite_action.setTexture(Ressources.TEXTURE.getTexture(TEXTURE.CHEST));
 		setTextureChest();
 	}
-	
+	public void setPosSprite(Vector2f pos) 
+	{
+		sprite.setPosition(pos);
+	    sprite_action.setPosition(pos.x,pos.y-60);
+	}
 	public void setTextureChest() 
 	{
-		sprite.setTextureRect(new IntRect(1, 1, 81, 81));
+		sprite.setTextureRect(new IntRect(1, 82, 80, 80));
+		sprite_action.setTextureRect(new IntRect(1, 1, 80, 80));
+	}
+	public void afficher() 
+	{
+		Graphic.SFML.draw(sprite);
+		Graphic.SFML.draw(sprite_action);
 	}
 }
