3439d6cbae157a6b0dea59636bc09a856c9ce67e
QuentinFA <aoghar@gmail.com>
2015-06-19 09:26:30 +0200

    Légère optimisation des blocs de couleur. MAJ test.xml.

--- src/Entities/ColorBlock.java
+++ src/Entities/ColorBlock.java
@@ -1,7 +1,16 @@
 package Entities;
 
+import Prog.Color;
+
 
 public abstract class ColorBlock extends Block
 {
-	
+	protected Color color;
+	protected int sprite_pos;
+
+	@Override
+	public void perform(Character p)
+	{
+		p.setColor(this.color);
+	}
 }
